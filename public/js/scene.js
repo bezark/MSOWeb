29a71ac4816405765ff11c76635c339c80577456
zebark <johnbezark@gmail.com>
2018-07-02 18:05:44 -0400

    all donked
    it's all donked now

--- public/js/scene.js
+++ public/js/scene.js
@@ -22,7 +22,7 @@ function init() {
   group = load_a_random_group()
   scene.add( group );
   hudSceneGen();
-  console.log(sceneOrtho);
+  // console.log(sceneOrtho);
 
   renderer = new THREE.WebGLRenderer();
   renderer.setPixelRatio( window.devicePixelRatio );
@@ -55,6 +55,70 @@ function onWindowResize() {
 
 ///HUD////
 
+// let sprite
+
+
+
+function hudSceneGen(){
+
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  cameraOrtho = new THREE.OrthographicCamera( - width / 2, width / 2, height / 2, - height / 2, 1, 5 );
+  cameraOrtho.position.z = 10;
+
+  sceneOrtho = new THREE.Scene();
+  var spriteMap = new THREE.TextureLoader().load( "assets/phrases/joke.png" );
+  var spriteMaterial = new THREE.SpriteMaterial( { map: spriteMap, color: 0xffffff } );
+  sprite = new THREE.Sprite( spriteMaterial );
+  spriteTL  .center.set( 0.0, 1.0 );
+  spriteTL.scale.set( width, height, 1 );
+  sprite.scale.set(1, 1, 1);
+  sceneOrtho.add( sprite );
+
+  updateHUDSprites();
+
+}
+
+function hudResize() {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
+  cameraOrtho.left = - width / 2;
+  cameraOrtho.right = width / 2;
+  cameraOrtho.top = height / 2;
+  cameraOrtho.bottom = - height / 2;
+  cameraOrtho.updateProjectionMatrix();
+
+  updateHUDSprites();
+
+
+}
+
+// function createHUDSprites ( texture ) {
+//
+//
+//   var material = new THREE.SpriteMaterial( { map: texture } );
+//
+//   var width = material.map.image.width;
+//   var height = material.map.image.height;
+//
+//   spriteTL = new THREE.Sprite( material );
+//   spriteTL.center.set( 0.0, 1.0 );
+//   spriteTL.scale.set( width, height, 1 );
+//   sceneOrtho.add( spriteTL );
+//   updateHUDSprites();
+// }
+
+
+function updateHUDSprites() {
+
+  var width = window.innerWidth / 2;
+  var height = window.innerHeight / 2;
+  sprite.position.set(   0,        0, -1 );
+  console.log("HUD SPRITE:");
+  console.log(sprite);
+  // center
+}
 
 /////////EVENTS////////////////////////
 
@@ -89,7 +153,7 @@ function animate() {
 
 function render() {
   renderer.clear();
-  renderer.render( scene, camera );
+  // renderer.render( scene, camera );
   renderer.clearDepth();
   renderer.render( sceneOrtho, cameraOrtho );
 }
