c3e2c91334d36680ebf4beca89d2652a1c2bcac4
littlestray <whatalunatik@gmail.com>
2018-09-05 18:07:04 -0400

    Audio V 1.0 for Production
    Mastered with phones in mind.

--- public/js/music.js
+++ public/js/music.js
@@ -1,14 +1,19 @@
 console.log("<CHRIS> music.js has loaded");
 
+let mySound;
+
 function preload() {
     // soundFormats('mp3', 'ogg');
-    mySound = loadSound('assets/audio/V1.0-MSOMusic_22-For-Web.mp3');
+    mySound = loadSound('assets/audio/V1.0-MSOMusic_25-ForWeb.mp3');
   }
   
   function setup() {
-    mySound.setVolume(1.3);
+    
+    mySound.setVolume(1);
     mySound.setLoop(true);
     mySound.play();
+    
     console.log("<CHRIS> MUSIC LOADED AND SET TO LOOP: " + mySound.isLooping());
     
   }
+
